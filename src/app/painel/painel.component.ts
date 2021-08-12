1659e64e90ded09bc40aa23c499bff5d2599cfb6
aline cardoso <alinedsc1989@gmail.com>
2021-08-12 03:14:08 -0300

    comparando resposta do usuario

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,10 +11,33 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a frase:"
+  public resposta: string = ""
+  public rodada: number = 0
+  public rodadaFrase: Frase
    
-  constructor() { console.log(this.frases) }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+     console.log(this.frases) 
+    }
 
   ngOnInit(): void {
+  }
+
+ public atualizaResposta(res: Event): void {
+   this.resposta = (<HTMLInputElement>res.target).value
+    console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    console.log('verificar resposta: ',this.resposta)
+
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert("A tradução está correta!")
+       //trocar pergunta da rodada:
+      this.rodada++
+      this.rodadaFrase = this.frases[this.rodada]
+    }
+  
   }
 
 }
